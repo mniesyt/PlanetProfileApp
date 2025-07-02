a9784d1e6a7e283dd78c1550db4ad8828fa7b529
Mara Niesyt <92339693+mniesyt@users.noreply.github.com>
2025-07-02 16:52:29 +0000

    This is the first test of PlanetProfileApp. Just Testing initial dropdowns

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,24 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Home", 'Settings'], 
+        icons=['house', 'gear'], menu_icon="cast", default_index=1)
+    selected
+
+st.title("Planet Profile")
+st.write("Let's Start by Setting Up Your Planet")
+st.selectbox("Choose your Planetary Body below",
+             ("Europa", "Ganymede", "Titan"))
+
+# will eventually have to have an actual call to the list of available moons from planet profile directly
+
+st.number_input("Select Your Bottom Temperature (in  $^\circ K$)")
+# User Passes in a Temperature of the bottom of the ocean
+
+
+st.selectbox("Choose your ocean composition",
+             ("Gibbs SeaWater", "NiSO4"))
+#User selects which water they want to use, will have to call from prespecified options list
+
+
