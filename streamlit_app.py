a541b2158674e53c360a5871337181fadcf40956
Mara Niesyt <92339693+mniesyt@users.noreply.github.com>
2025-07-02 20:53:26 +0000

    Added in option to select up to three salts and concentrations

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,45 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
 
-with st.sidebar:
-    selected = option_menu("Main Menu", ["Home", 'Settings'], 
-        icons=['house', 'gear'], menu_icon="cast", default_index=1)
-    selected
+#from streamlit_option_menu import option_menu
 
 st.title("Planet Profile")
 st.write("Let's Start by Setting Up Your Planet")
+
+st.subheader("Use Predefined Body?")
+
 st.selectbox("Choose your Planetary Body below",
              ("Europa", "Ganymede", "Titan"))
 
 # will eventually have to have an actual call to the list of available moons from planet profile directly
-
+st.subheader("Temperature Setup")
 st.number_input("Select Your Bottom Temperature (in  $^\circ K$)")
 # User Passes in a Temperature of the bottom of the ocean
 
-
-st.selectbox("Choose your ocean composition",
-             ("Gibbs SeaWater", "NiSO4"))
+st.subheader("Ocean Composition")
+user_ocean_type = st.selectbox("Use Predefined Ocean or Define own Ocean Composition", ("Use pre-defined ocean composition","Define your own ocean composition"),index = None, placeholder = "Select Ocean Type")
 #User selects which water they want to use, will have to call from prespecified options list
+#print(user_ocean_type.index)
 
+if user_ocean_type == "Use pre-defined ocean composition":
+   st.write("Using Predefined Ocean")
+
+
+if user_ocean_type == "Define your own ocean composition":
+    st.write("Define Your Ocean Below")
+# will have to add an option to select concentration units  
+    num_salts = st.number_input("Input number of salt species", min_value = 1, max_value = 3)
+    st.text_input("First Ocean Salt")
+    st.number_input("First Salt Concentration")
+
+    if num_salts >= 2: 
+        st.text_input("Second Ocean Salt")
+        st.number_input("Second Salt Concentration")
+
+    if num_salts >= 3: 
+        st.text_input("Third Ocean Salt")
+        st.number_input("Third Salt Concentration")
+
+#with st.popover("Open popover"):
+   # st.markdown("Hello World 👋")
+    #name = st.text_input("What's your name?")
 
