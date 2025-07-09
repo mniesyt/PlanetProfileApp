f0473e115681910a0dc528b741789a4ee2146156
Mara Niesyt <92339693+mniesyt@users.noreply.github.com>
2025-07-09 21:15:28 +0000

    testing sys.path.append

--- pages/BulkPlanetarySettings.py
+++ pages/BulkPlanetarySettings.py
@@ -15,11 +15,11 @@ Planet = os.getenv("Planet")
 # For /PlanetProfile/PlanetProfileApp/your_script.py, this will be:
 # /PlanetProfile/PlanetProfileApp
 BulkPlanertarySettings_directory = os.path.dirname(os.path.abspath(__file__))
-st.write(BulkPlanertarySettings_directory)
+#st.write(BulkPlanertarySettings_directory)
 
 # Get the parent directory (/PlanetProfile)
 app_directory = os.path.dirname(BulkPlanertarySettings_directory)
-st.write(app_directory)
+#st.write(app_directory)
 parent_directory  = os.path.dirname(app_directory)
 st.write(parent_directory)
 # Add the parent directory to Python's search path.
@@ -28,11 +28,11 @@ if parent_directory not in sys.path:
     sys.path.append(parent_directory)
 
 
-#current_directory = os.getcwd()
-#st.write(current_directory)
 
-#os.chdir('..') #From the PlanetProfile/PlanetProfileApp, going to PlanetProfile
+os.chdir('..') #From the PlanetProfile/PlanetProfileApp, going to PlanetProfile
 #st.write(parent_directory)
+current_directory = os.getcwd()
+st.write(current_directory)
 
 #os.chdir(str(Planet)) #now in the individual folder of the planet
 #st.write(os.getcwd())
@@ -51,10 +51,15 @@ Planet = os.getenv("Planet") # e.g., "Venus"
 # e.g., if Planet is "Venus", this becomes "PPVenus"
 module_to_import = f"PP{Planet}"
 
-# Use importlib to import the module
-planet_module = importlib.import_module(module_to_import)
 
-st.write(planet_module)
+# Use importlib to import the module
+st.write(sys.path)
+planet_folder = sys.path.append(Planet)
+st.write(planet_folder)
+
+#planet_module = importlib.import_module(planet_folder.module_to_import)
+
+#st.write(planet_module)
 
 #os.environ["Planet.Bulk.R_m"] = str(st.number_input("Radius of the body (m)", value = planet_module.Planet.Bulk.R_m))
 #to be passed to Planet.Bulk.R_m
