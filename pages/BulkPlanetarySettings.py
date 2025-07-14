be53466198782d409b934f3c08774c97218024fd
Mara Niesyt <92339693+mniesyt@users.noreply.github.com>
2025-07-14 18:04:34 +0000

    testing keys

--- pages/BulkPlanetarySettings.py
+++ pages/BulkPlanetarySettings.py
@@ -59,26 +59,26 @@ Planet =PlanetStruct(Planet)
 
 # Initializing the session state of all the variables
 if "Planet.Bulk.R_m" not in st.session_state:
-    st.session_state.Planet_Bulk_R_m = planet_module.Planet.Bulk.R_m  # Initialize the session state
+    st.session_state["Planet.Bulk.R_m"]= planet_module.Planet.Bulk.R_m  # Initialize the session state
 if "Planet.Bulk.M_kg" not in st.session_state:
-    st.session_state.Planet_Bulk_M_kg = planet_module.Planet.Bulk.M_kg
+    st.session_state["Planet.Bulk.M_kg"] = planet_module.Planet.Bulk.M_kg
 if "Planet.Bulk.Tsurf_K" not in st.session_state:
-     st.session_state.Planet_Bulk_Tsurf_K = planet_module.Planet.Bulk.Tsurf_K
+     st.session_state["Planet.Bulk.Tsurf_K"] = planet_module.Planet.Bulk.Tsurf_K
 if "Planet.Bulk.Psurf_MPa" not in st.session_state:
-    st.session_state.Planet_Bulk_Psurf_MPa = planet_module.Planet.Bulk.Psurf_MPa
+    st.session_state["Planet.Bulk.Psurf_MPa"] = planet_module.Planet.Bulk.Psurf_MPa
 if "Planet.Bulk.Cmeasured" not in st.session_state:
-    st.session_state.Planet_Bulk_Cmeasured = planet_module.Planet.Bulk.Cmeasured
+    st.session_state["Planet.Bulk.Cmeasured"] = planet_module.Planet.Bulk.Cmeasured
 if "Planet.Bulk.Cuncertainty" not in st.session_state:
-    st.session_state.Planet_Bulk_Cuncertainty = planet_module.Planet.Bulk.Cuncertainty
+    st.session_state["Planet.Bulk.Cuncertainty"] = planet_module.Planet.Bulk.Cuncertainty
 if "Planet.Bulk.Tb_K" not in st.session_state:
-    st.session_state.Planet_Bulk_Tb_K = planet_module.Planet.Bulk.Tb_K
+    st.session_state["Planet.Bulk.Tb_K"] = planet_module.Planet.Bulk.Tb_K
 
 
 def user_input_a_variable(bulk_setting):
     st.write("You are setting a custom value for this variable: " + bulk_setting)
 
 
-Planet.Bulk.R_m = st.number_input("Radius of the body (m)", value = planet_module.Planet.Bulk.R_m, key = "Planet.Bulk.R_m", on_change = user_input_a_variable("Radius of the body (m)"))
+Planet.Bulk.R_m = st.number_input("Radius of the body (m)",  key = "Planet.Bulk.R_m", on_change = user_input_a_variable("Radius of the body (m)"))
 Planet.Bulk.M_kg = st.number_input("Mass of the body (kg)", value = planet_module.Planet.Bulk.M_kg, key = "Planet.Bulk.M_kg", on_change = user_input_a_variable("Mass of the body (kg)"))
 Planet.Bulk.Tsurf_K = st.number_input("Temperature at the surface ($^\circ K$)", value = planet_module.Planet.Bulk.Tsurf_K, key = "Planet.Bulk.Tsurf_K",on_change = user_input_a_variable("Temperature at the surface ($^\circ K$)"))
 Planet.Bulk.Psurf_MPa = st.number_input("Pressure at the surface (MPa)", value = planet_module.Planet.Bulk.Psurf_MPa, key = "Planet.Bulk.Psurf_MPa", on_change = user_input_a_variable("Pressure at the surface (MPa)"))
