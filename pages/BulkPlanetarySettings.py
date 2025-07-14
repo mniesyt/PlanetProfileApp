2ca1f0d9cc552f6705e9296ec826c96e01816095
Mara Niesyt <92339693+mniesyt@users.noreply.github.com>
2025-07-14 17:51:15 +0000

    Testing keys imn initialization of planet bulk settings

--- pages/BulkPlanetarySettings.py
+++ pages/BulkPlanetarySettings.py
@@ -60,20 +60,31 @@ Planet =PlanetStruct(Planet)
 # Initializing the session state of all the variables
 if "Planet.Bulk.R_m" not in st.session_state:
     st.session_state.Planet_Bulk_R_m = planet_module.Planet.Bulk.R_m  # Initialize the session state
-
+if "Planet.Bulk.M_kg" not in st.session_state:
+    st.session_state.Planet_Bulk_M_kg = planet_module.Bulk.M_kg
+if "Planet.Bulk.Tsurf_K" not in st.session_state:
+     st.session_state.Planet_Bulk_Tsurf_K = planet_module.Bulk.Tsurf_K
+if "Planet.Bulk.Psurf_MPa" not in st.session_state:
+    st.session_state.Planet_Bulk_Psurf_MPa = planet_module.Bulk.Psurf_MPa
+if "Planet.Bulk.Cmeasured" not in st.session_state:
+    st.session_state.Planet_Bulk_Cmeasured = Planet.Bulk.Cmeasured
+if "Planet.Bulk.Cuncertainty" not in st.session_state:
+    st.session_state.Planet_Bulk_Cuncertainty = Planet.Bulk.Cuncertainty
+if "Planet.Bulk.Tb_K" not in st.session_state:
+    st.session_state.Planet_Bulk_Tb_K = Planet.Bulk.Tb_K
 
 
 def user_input_a_variable(bulk_setting):
     st.write("You are setting a custom value for this variable: " + bulk_setting)
 
 
-Planet.Bulk.R_m = st.number_input("Radius of the body (m)", key = "Planet.Bulk.R_m", on_change = user_input_a_variable("Radius of the body (m)"))
-Planet.Bulk.M_kg = st.number_input("Mass of the body (kg)", value = planet_module.Planet.Bulk.M_kg, on_change = user_input_a_variable("Mass of the body (kg)"))
-Planet.Bulk.Tsurf_K = st.number_input("Temperature at the surface ($^\circ K$)", value = planet_module.Planet.Bulk.Tsurf_K, on_change = user_input_a_variable("Temperature at the surface ($^\circ K$)"))
-Planet.Bulk.Psurf_MPa = st.number_input("Pressure at the surface (MPa)", value = planet_module.Planet.Bulk.Psurf_MPa, on_change = user_input_a_variable("Pressure at the surface (MPa)"))
-Planet.Bulk.Cmeasured = st.number_input("Normalized Axial Moment of Inertia $C$", value = planet_module.Planet.Bulk.Cmeasured, on_change = user_input_a_variable("Normalized Axial Moment of Inertia $C$"))
-Planet.Bulk.Cuncertainty = st.number_input("Uncertainty in $C$", value = planet_module.Planet.Bulk.Cuncertainty, on_change = user_input_a_variable("Uncertainty in $C$y"))
-Planet.Bulk.Tb_K = st.number_input("Temperature at the bottom ($^\circ K$)", value = planet_module.Planet.Bulk.Tb_K, on_change = user_input_a_variable("Temperature at the bottom "))
+Planet.Bulk.R_m = st.number_input("Radius of the body (m)", value = planet_module.Planet.Bulk.R_m, key = "Planet.Bulk.R_m", on_change = user_input_a_variable("Radius of the body (m)"))
+Planet.Bulk.M_kg = st.number_input("Mass of the body (kg)", value = planet_module.Planet.Bulk.M_kg, key = "Planet.Bulk.M_kg", on_change = user_input_a_variable("Mass of the body (kg)"))
+Planet.Bulk.Tsurf_K = st.number_input("Temperature at the surface ($^\circ K$)", value = planet_module.Planet.Bulk.Tsurf_K, key = "Planet.Bulk.M_kg",on_change = user_input_a_variable("Temperature at the surface ($^\circ K$)"))
+Planet.Bulk.Psurf_MPa = st.number_input("Pressure at the surface (MPa)", value = planet_module.Planet.Bulk.Psurf_MPa, key = "Bulk.Psurf_MPa", on_change = user_input_a_variable("Pressure at the surface (MPa)"))
+Planet.Bulk.Cmeasured = st.number_input("Normalized Axial Moment of Inertia $C$", value = planet_module.Planet.Bulk.Cmeasured, key = "Planet.Bulk.Cmeasured", on_change = user_input_a_variable("Normalized Axial Moment of Inertia $C$"))
+Planet.Bulk.Cuncertainty = st.number_input("Uncertainty in $C$", value = planet_module.Planet.Bulk.Cuncertainty, key = "Planet.Bulk.Cuncertainty", on_change = user_input_a_variable("Uncertainty in $C$y"))
+Planet.Bulk.Tb_K = st.number_input("Temperature at the bottom ($^\circ K$)", value = planet_module.Planet.Bulk.Tb_K, key = "Planet.Bulk.Tb_K", on_change = user_input_a_variable("Temperature at the bottom "))
 #os.environ["Planet.Bulk.Tb_K"] = str(st.number_input("Temperature at the bottom ($^\circ K$)", value = planet_module.Planet.Bulk.Tb_K))
 
 #need to make it so that custom runs save to /PlanetProfile/Planet, and that these runs will be available for the user to load their custom runs later
