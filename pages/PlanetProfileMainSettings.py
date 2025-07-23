c783af16f9f5810081d2d92cabc54ec3387494f2
Mara Niesyt <92339693+mniesyt@users.noreply.github.com>
2025-07-23 18:21:38 +0000

    testing Planet.Do

--- pages/PlanetProfileMainSettings.py
+++ pages/PlanetProfileMainSettings.py
@@ -122,15 +122,15 @@ if thickness_or_Tb == "Input Ice Shell thickness":
     st.number_input("Select the thickness of your Ice I Shell (in  $m$) below")
     st.write("Planet Profile will use the inputted ice layer thickness to generate the ice shell for your planet. Based on the ice shell thickness, the temperature at the bottom of the Ice shell will be calculated")
 
-    st.write(Planet)
+
     # Ensure Planet.Do exists and is a dictionary
-    if not hasattr(Planet, "Do") or not isinstance(Planet.Do, dict):
-        Planet.Do = {}
-    # Now safely set ICEIh_THICKNESS
-    Planet.Do["ICEIh_THICKNESS"] = True #user is inputting the thickness instead of Tb_K so this flag is set to true
-    
-    #Planet.Do.ICEIh_THICKNESS = True 
+    if not hasattr(Planet.Do, "ICEIh_THICKNESS"):
+        Planet.Do.ICEIh_THICKNESS = True
+    else:
+        Planet.Do.ICEIh_THICKNESS = True  #  #user is inputting the thickness instead of Tb_K so this flag is set to true
+
     st.write(Planet.Do.ICEIh_THICKNESS)
+    st.write(Planet)
     #Planet.Bulk.zb_approximate_km = 30 # The approximate ice shell thickness desired (edited) 
 
 
